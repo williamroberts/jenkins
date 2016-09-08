6979d7da5c3839e07a6ffa0cdfbfd4f6f33eb12b
Will Roberts <williamroberts@live.co.uk>
2016-09-09 08:39:16 +1000

    Correct use of non-existent method for get env vars... me dumb dumb

--- groovy-scripts/script_2_set_jenkins_url.groovy
+++ groovy-scripts/script_2_set_jenkins_url.groovy
@@ -1,6 +1,6 @@
 import jenkins.model.JenkinsLocationConfiguration
 
-def jenkinsUrl = System.envvar("JENKINS_URL")
+def jenkinsUrl = System.getenv("JENKINS_URL")
 def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
 jenkinsLocationConfiguration.setUrl(jenkinsUrl)
 jenkinsLocationConfiguration.save()
